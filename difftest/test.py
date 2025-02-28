fce588fdc6a9e8c04aba0ac76df037363c6d6b5a
EPTansuo <eptansuo@163.com>
2025-02-28 20:51:57 +0800

    del some useless info

--- difftest/test.py
+++ difftest/test.py
@@ -1,15 +1,15 @@
 import sys
 
 import sys
-sys.path.append("../lib")  # 添加模块所在目录
-sys.path.append("./lib")  # 添加模块所在目录
+sys.path.append("../lib")
+sys.path.append("./lib")
 
 from pstring import str as pstr
 
 s = pstr("hello world")
 
-print(s.upper())       # 输出HELLO WORLD
-print(s.split(' '))    # 输出['hello', 'world']
-print(s[1:5])          # 切片输出'ello'
+print(s.upper())
+print(s.split(' '))
+print(s[1:5])
 
 
