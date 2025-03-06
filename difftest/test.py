99b29ec059cc1063228a6512467fcd4ceed5510e
EPTansuo <eptansuo@163.com>
2025-03-06 10:10:21 +0800

    add more test and find some bugs

--- difftest/test.py
+++ difftest/test.py
@@ -1,14 +1,21 @@
 import sys
 import unicodedata
+import random
+import string
 sys.path.extend(["../lib", "./lib"])
 
 from pstring import pstr
 
-def difftest(ref, impl, method_name, verbose=True):
+def random_str():
+    return  ''.join(random.sample(10*(string.ascii_letters + string.digits + " \v\f\n\t "), random.randint(0,100)))
+
+
+def difftest(test_str, ref, impl, method_name, verbose=True):
     try:
         if ref != impl:
             print(f"\033[31mFAIL\033[0m: {method_name}()")
             if verbose:
+                print(f"  Test Case: {repr(test_str)}")
                 print(f"  Reference: {repr(ref)}")
                 print(f"  Implementation: {repr(impl)}")
             return False
@@ -28,76 +35,79 @@ def run_tests():
         ("'\"\\", "Escape characters"),
     ]
 
+    for i in range(100):
+        test_cases.append(tuple([random_str(), f"random_test{i}"]))
+
     for test_str, case_name in test_cases:
         print(f"\n\033[34m=== Test: {case_name} ===\033[0m")
         py_str = test_str
         ps_str = pstr(test_str)
 
-        difftest(py_str.upper(), ps_str.upper(), "upper")
-        difftest(py_str.lower(), ps_str.lower(), "lower")
-        difftest(py_str.title(), ps_str.title(), "title")
-        difftest(py_str.capitalize(), ps_str.capitalize(), "capitalize")
-        difftest(py_str.swapcase(), ps_str.swapcase(), "swapcase")
-        difftest(py_str.casefold(), ps_str.casefold(), "casefold")  # Unicode casefolding
+        difftest(test_str, py_str.upper(), ps_str.upper(), "upper")
+        difftest(test_str, py_str.lower(), ps_str.lower(), "lower")
+        difftest(test_str, py_str.title(), ps_str.title(), "title")
+        difftest(test_str, py_str.capitalize(), ps_str.capitalize(), "capitalize")
+        difftest(test_str, py_str.swapcase(), ps_str.swapcase(), "swapcase")
+        difftest(test_str, py_str.casefold(), ps_str.casefold(), "casefold")  # Unicode casefolding
         
-        difftest(py_str.strip(), ps_str.strip(), "strip")
-        difftest(py_str.lstrip(), ps_str.lstrip(), "lstrip")
-        difftest(py_str.rstrip(), ps_str.rstrip(), "rstrip")
-        difftest(py_str.removeprefix(" \r\n\t"), ps_str.removeprefix(" \r\n\t"), "removeprefix")
-        difftest(py_str.removesuffix("\t\r\n "), ps_str.removesuffix("\t\r\n "), "removesuffix")
+        difftest(test_str, py_str.strip(), ps_str.strip(), "strip")
+        difftest(test_str, py_str.lstrip(), ps_str.lstrip(), "lstrip")
+        difftest(test_str, py_str.rstrip(), ps_str.rstrip(), "rstrip")
+        difftest(test_str, py_str.removeprefix(" \r\n\t"), ps_str.removeprefix(" \r\n\t"), "removeprefix")
+        difftest(test_str, py_str.removesuffix("\t\r\n "), ps_str.removesuffix("\t\r\n "), "removesuffix")
 
 
-        difftest(py_str.split(), ps_str.split(), "split")
-        difftest(py_str.split(maxsplit=2), ps_str.split(maxsplit=2), "split(maxsplit=2)")
-        difftest(py_str.rsplit(maxsplit=1), ps_str.rsplit(maxsplit=1), "rsplit(maxsplit=1)")
-        difftest(py_str.splitlines(), ps_str.splitlines(), "splitlines")
-        difftest(py_str.splitlines(False), ps_str.splitlines(False), "splitlines")
-        difftest(py_str.splitlines(True), ps_str.splitlines(True), "splitlines")
+        difftest(test_str, py_str.split(), ps_str.split(), "split")
+        difftest(test_str, py_str.split(maxsplit=2), ps_str.split(maxsplit=2), "split(maxsplit=2)")
+        difftest(test_str, py_str.rsplit(maxsplit=1), ps_str.rsplit(maxsplit=1), "rsplit(maxsplit=1)")
+        difftest(test_str, py_str.splitlines(), ps_str.splitlines(), "splitlines")
+        difftest(test_str, py_str.splitlines(False), ps_str.splitlines(False), "splitlines")
+        difftest(test_str, py_str.splitlines(True), ps_str.splitlines(True), "splitlines")
         
-        difftest(py_str.center(20, '*'), ps_str.center(20, '*'), "center")
-        difftest(py_str.ljust(15), ps_str.ljust(15), "ljust")
-        difftest(py_str.rjust(15, '#'), ps_str.rjust(15, '#'), "rjust")
-        difftest(py_str.zfill(10), ps_str.zfill(10), "zfill")
+        difftest(test_str, py_str.center(20, '*'), ps_str.center(20, '*'), "center")
+        difftest(test_str, py_str.ljust(15), ps_str.ljust(15), "ljust")
+        difftest(test_str, py_str.rjust(15, '#'), ps_str.rjust(15, '#'), "rjust")
+        difftest(test_str, py_str.zfill(10), ps_str.zfill(10), "zfill")
         
-        difftest(py_str.find('e'), ps_str.find('e'), "find")
-        difftest(py_str.rfind('l'), ps_str.rfind('l'), "rfind")
+        difftest(test_str, py_str.find('e'), ps_str.find('e'), "find")
+        difftest(test_str, py_str.rfind('l'), ps_str.rfind('l'), "rfind")
 
         try:
             pstr_index = ps_str.index('e')
             py_index = py_str.index('e')
-            difftest(py_index, pstr_index, "index")
+            difftest(test_str, py_index, pstr_index, "index")
         except ValueError:
             pass
 
-        difftest(py_str.count('l'), ps_str.count('l'), "count")
-        difftest(py_str.replace('l', 'L', 2), ps_str.replace('l', 'L', 2), "replace")
+        difftest(test_str, py_str.count('l'), ps_str.count('l'), "count")
+        difftest(test_str, py_str.replace('l', 'L', 2), ps_str.replace('l', 'L', 2), "replace")
         
-        difftest(py_str.startswith(('He', 'ha')), ps_str.startswith(('He', 'ha')), "startswith")
-        difftest(py_str.startswith('ha'), ps_str.startswith('ha'), "startswith")
-        difftest(py_str.startswith('He'), ps_str.startswith('He'), "startswith")
-        difftest(py_str.endswith(('ld', 'LD')), ps_str.endswith(('ld', 'LD')), "endswith")
-        difftest(py_str.endswith('LD'), ps_str.endswith('LD'), "endswith")
-        difftest(py_str.endswith('ld'), ps_str.endswith('ld'), "endswith")
-        difftest(py_str.endswith(' '), ps_str.endswith(' '), "endswith")
+        difftest(test_str, py_str.startswith(('He', 'ha')), ps_str.startswith(('He', 'ha')), "startswith")
+        difftest(test_str, py_str.startswith('ha'), ps_str.startswith('ha'), "startswith")
+        difftest(test_str, py_str.startswith('He'), ps_str.startswith('He'), "startswith")
+        difftest(test_str, py_str.endswith(('ld', 'LD')), ps_str.endswith(('ld', 'LD')), "endswith")
+        difftest(test_str, py_str.endswith('LD'), ps_str.endswith('LD'), "endswith")
+        difftest(test_str, py_str.endswith('ld'), ps_str.endswith('ld'), "endswith")
+        difftest(test_str, py_str.endswith(' '), ps_str.endswith(' '), "endswith")
 
-        difftest(py_str.expandtabs(2), ps_str.expandtabs(2), "expandtabs")
-        difftest(py_str.expandtabs(), ps_str.expandtabs(), "expandtabs")
-        difftest(py_str.expandtabs(0), ps_str.expandtabs(0), "expandtabs")
+        difftest(test_str, py_str.expandtabs(2), ps_str.expandtabs(2), "expandtabs")
+        difftest(test_str, py_str.expandtabs(), ps_str.expandtabs(), "expandtabs")
+        difftest(test_str, py_str.expandtabs(0), ps_str.expandtabs(0), "expandtabs")
         
         if hasattr(ps_str, 'format'):
             fmt_str = pstr("{} + {} = {}")
-            difftest("1 + 2 = 3", fmt_str.format(1, 2, 3), "format")
+            difftest(test_str, "1 + 2 = 3", fmt_str.format(1, 2, 3), "format")
         
         try:
             encoded_ref = py_str.encode('utf-8')
             encoded_impl = ps_str.encode('utf-8')
-            difftest(encoded_ref, bytes(encoded_impl), "encode")
+            difftest(test_str, encoded_ref, bytes(encoded_impl), "encode")
         except AttributeError:
             pass  
         
-        difftest(py_str * 3, ps_str * 3, "*")
-        difftest(py_str + py_str, ps_str + ps_str, "+")
-        difftest(py_str[1:-1], ps_str[1:-1], "silice")
+        difftest(test_str, py_str * 3, ps_str * 3, "*")
+        difftest(test_str, py_str + py_str, ps_str + ps_str, "+")
+        difftest(test_str, py_str[1:-1], ps_str[1:-1], "silice")
         
         # nfc_str = unicodedata.normalize('NFC', py_str)
         # nfd_str = unicodedata.normalize('NFD', py_str)
